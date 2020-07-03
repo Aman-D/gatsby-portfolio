c78f4269dec1009ba1ed133b8354f69e81df9574
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-03 16:47:24 +0530

    Configure MDX

--- src/templates/pages.jsx
+++ src/templates/pages.jsx
@@ -1,12 +1,48 @@
 import React from "react"
 import * as allComponents from "../components/index"
+import { MDXComponents as mdxComponents } from "../mdx-components"
+import {
+  Heading,
+  Link,
+  Stack,
+  FormControl,
+  Input,
+  Button,
+  Flex,
+  Text,
+  Box,
+  Icon,
+  SimpleGrid,
+  Image,
+  IconButton,
+  AspectRatioBox,
+} from "@chakra-ui/core"
+
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
-const Pages = () => {
+const Pages = ({ data: { mdx } }) => {
+  const MDXComponents = {
+    ...allComponents,
+    ...mdxComponents,
+    Link,
+    Box,
+    SimpleGrid,
+    Image,
+    Flex,
+    IconButton,
+    Icon,
+    AspectRatioBox,
+    Button,
+    FormControl,
+    Input,
+    Stack,
+    Text,
+    Heading,
+  }
   return (
     <allComponents.Layout>
-      <MDXProvider>
+      <MDXProvider components={MDXComponents}>
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </MDXProvider>
     </allComponents.Layout>
@@ -14,3 +50,16 @@ const Pages = () => {
 }
 
 export default Pages
+
+export const pageQuery = graphql`
+  query page($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      body
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`
