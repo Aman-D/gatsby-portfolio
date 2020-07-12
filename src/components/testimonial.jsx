9e96403257f7a931c1dcb7e1fe548ae144450243
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-12 16:13:02 +0530

    New testimonial component

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -2,57 +2,14 @@ import React, { useState } from "react"
 import { Box, Flex, Text, Image } from "@chakra-ui/core"
 import { motion } from "framer-motion"
 import { MdNavigateNext, MdNavigateBefore } from "react-icons/md"
+import { testimonial } from "../testimonial"
 const Testimonial = ({ children, ...rest }) => {
   const MotionCard = motion.custom(Flex)
 
-  const Card = ({ children, name, position, ...rest }) => {
-    return (
-      <MotionCard
-        direction="column"
-        boxShadow="xl"
-        p={4}
-        maxW={["320 !important", "420px !important"]}
-        minW={["320 !important", "420px !important"]}
-        initial={{ scale: 0, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        {...rest}
-      >
-        {children.map(({ props }, index) => {
-          if (props.mdxType === "img") {
-            return (
-              <Image
-                key={index}
-                src={props.src}
-                size={20}
-                borderRadius="50%"
-                objectFit="cover"
-                alignSelf="center"
-                border="2px solid white"
-              />
-            )
-          }
-
-          if (props.mdxType === "p") {
-            return (
-              <Flex flexDirection="column" textAlign="center" key={index}>
-                <Text p={4}>
-                  {name},{position}
-                </Text>
-
-                <Text>{props.children}</Text>
-              </Flex>
-            )
-          }
-        })}
-      </MotionCard>
-    )
-  }
-
-  children = !children.length ? [children] : children
   const [tabIndex, setIndex] = useState(0)
 
   const next = () => {
-    if (tabIndex < children.length - 1) {
+    if (tabIndex < testimonial.length - 1) {
       setIndex(tabIndex + 1)
     }
   }
@@ -63,21 +20,38 @@ const Testimonial = ({ children, ...rest }) => {
     }
   }
   return (
-    <Box {...rest} position="relative">
+    <Box position="relative" {...rest}>
       <>
-        {children.map(({ props }, index) => {
-          if (props.mdxType === "Box") {
-            return (
-              <Card
-                display={tabIndex === index ? "flex" : "none"}
-                key={index}
-                name={props.name}
-                position={props.position}
-              >
-                {props.children}
-              </Card>
-            )
-          }
+        {testimonial.map(({ id, name, position, words, img }, index) => {
+          return (
+            <MotionCard
+              display={tabIndex === index ? "flex" : "none"}
+              direction="column"
+              boxShadow="xl"
+              p={4}
+              maxW={["320 !important", "420px !important"]}
+              minW={["320 !important", "420px !important"]}
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              {...rest}
+            >
+              <Image
+                src={img}
+                size={20}
+                borderRadius="50%"
+                objectFit="cover"
+                alignSelf="center"
+                border="2px solid white"
+              />
+              <Flex flexDirection="column" textAlign="center">
+                <Text p={4}>
+                  {name},{position}
+                </Text>
+
+                <Text>{words}</Text>
+              </Flex>
+            </MotionCard>
+          )
         })}
         <Box
           as={MdNavigateBefore}
